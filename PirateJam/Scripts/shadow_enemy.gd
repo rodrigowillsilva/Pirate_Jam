cdbcb43da72034015ad61b99f50a10c5811a17cd
VeyJhow <gfurlan39@gmail.com>
2024-07-30 22:10:15 -0300

    monsters added to the maps

--- PirateJam/Scripts/shadow_enemy.gd
+++ PirateJam/Scripts/shadow_enemy.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
-const speed : int = 100
+class_name monster_shadow
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var speed : int = 50
 var direction : Vector2
 var direction_array : Array = [Vector2.LEFT,Vector2.RIGHT]
 @onready var animated = $AnimatedSprite2D
 @onready var raycast_wall = $RayCast2D
 @onready var raycast_void = $RayCast2D2
+@onready var raycast_chase = $RayCast2D3
 @onready var random_stop_timer = $random_stop_timer
+var chasing : bool = false
+var Player
 
 func _ready():
+	Player = get_parent().find_child("Player")
 	direction = direction_array.pick_random()
 	random_stop_timer.wait_time = randi_range(5,20)
 	random_stop_timer.start()
@@ -23,9 +30,13 @@ func _process(_delta):
 		_stop_and_change()
 	if not raycast_void.is_colliding() and raycast_void.enabled == true:
 		_stop_and_change()
+	_change_state()
 
-func _physics_process(_delta):
-	velocity = direction * speed
+func _physics_process(delta):
+	if is_on_floor():
+		velocity = direction * speed
+	else:
+		velocity.y += gravity * delta
 	move_and_slide()
 
 func _check_direction():
@@ -33,10 +44,12 @@ func _check_direction():
 		animated.flip_h = true
 		raycast_wall.target_position = Vector2(0,-19)
 		raycast_void.target_position = Vector2(18,-20)
+		raycast_chase.target_position = Vector2(75,0)
 	elif direction == Vector2.LEFT:
 		animated.flip_h = false
 		raycast_wall.target_position = Vector2(0,19)
 		raycast_void.target_position = Vector2(18,20)
+		raycast_chase.target_position = Vector2(-75,0)
 
 func _stop_and_change():
 	raycast_wall.enabled = false
@@ -44,6 +57,7 @@ func _stop_and_change():
 	direction = Vector2.ZERO
 	animated.play("idle")
 	await animated.animation_finished
+	animated.play("walking")
 	raycast_wall.enabled = true
 	raycast_void.enabled = true
 	if animated.flip_h == true:
@@ -57,9 +71,24 @@ func _random_stop():
 	direction = Vector2.ZERO
 	animated.play("idle")
 	await animated.animation_finished
+	animated.play("walking")
 	raycast_wall.enabled = true
 	raycast_void.enabled = true
 	direction = direction_array.pick_random()
+
+func _change_state():
+	if raycast_chase.get_collider() == Player:
+		chasing = true
+		speed = 150
+		raycast_wall.enabled = false
+		raycast_void.enabled = false
+		random_stop_timer.stop()
+	elif raycast_chase.get_collider() != Player:
+		chasing = false
+		speed = 50
+		raycast_wall.enabled = true
+		raycast_void.enabled = true
+		random_stop_timer.stop()
 
 func _process_timer():
 	random_stop_timer.wait_time = randi_range(5,20)
--- PirateJam/furlan_levels/level_furlan3.gd
+++ PirateJam/furlan_levels/level_furlan3.gd
@@ -8,7 +8,11 @@ var checkpoint : Vector2
 @onready var checkpoint_area = $checkpoint_area
 @onready var end_camera = $end_camera_area
 @onready var level_end_area = $level_end_area
+@onready var hurt_area = $hurt_area
 
+
+func _process(_delta):
+	hurt_area.position = player.position
 
 func _on_checkpoint_area_body_entered(body):
 	if body is alchemist:
@@ -28,3 +32,7 @@ func _on_end_camera_area_body_entered(body):
 func _on_level_end_area_body_entered(body):
 	if body is alchemist:
 		get_tree().change_scene_to_file("res://furlan_levels/level_furlan4.tscn")
+
+func _on_hurt_area_body_entered(body):
+	if body is monster_shadow:
+		_go_to_checkpoint()
